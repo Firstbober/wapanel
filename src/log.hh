1e55303a890fbc058ef774351c4f35acc56c8f59
Firstbober <firstbober@tutanota.com>
2020-10-15 23:47:04 +0200

    Change info log color to blue.

--- src/log.hh
+++ src/log.hh
@@ -3,6 +3,7 @@
 
 #define _LOG_COLOR_RED	  "\033[31m"
 #define _LOG_COLOR_YELLOW "\033[33m"
+#define _LOG_COLOR_BLUE "\033[34m"
 #define _LOG_COLOR_RESET  "\033[0m"
 
 #define log_error(log, ...)                                                                                            \
@@ -13,4 +14,4 @@
 	fprintf(stderr, _LOG_COLOR_YELLOW "[wapanel] [WARN] (" __FILE__ ":%i): " log "\n" _LOG_COLOR_RESET, __LINE__,      \
 			##__VA_ARGS__)
 
-#define log_info(log, ...) fprintf(stderr, "[wapanel] [INFO] (" __FILE__ ":%i): " log "\n", __LINE__, ##__VA_ARGS__)
+#define log_info(log, ...) fprintf(stderr, _LOG_COLOR_BLUE "[wapanel] [INFO] (" __FILE__ ":%i): " log "\n" _LOG_COLOR_RESET, __LINE__, ##__VA_ARGS__)
